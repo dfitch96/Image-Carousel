753e3dceb83005d40ec72a2b94f0bb51d02941d3
Dylan Fitch <kkarl30@gmail.com>
2025-01-21 16:29:54 -0500

    finished most of functionality. Need to start working on calling next() on set intervals

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const imagePaths = [
     "images/mtg-trading-cards.png"
 ]
 
-let currentIndex = 0;
+
 const carouselImgDiv = document.querySelector(".carousel-image");
 const nextButton = document.querySelector('#next');
 const prevButton = document.querySelector('#previous')
+const carouselSelectionsDiv = document.querySelector(".carousel-selections");
+let currentIndex = 0;
+let currentSelected = carouselSelectionsDiv.children[0];
+currentSelected.classList.add('active');
+
 
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -17,16 +22,40 @@ window.addEventListener("DOMContentLoaded", () => {
     carouselImg.src = imagePaths[0];
     carouselImgDiv.appendChild(carouselImg);
 
+    for(let i = 0; i < carouselSelectionsDiv.children.length; i++){
+        carouselSelectionsDiv.children[i].dataset.index = i;
+        carouselSelectionsDiv.children[i].addEventListener("click", selectionHandler);
+
+    }
+
 })
 
 nextButton.addEventListener("click", next);
 prevButton.addEventListener("click", prev);
 
 
+function selectionHandler(event){
+
+    currentSelected.classList.remove('active');
+
+    currentIndex = event.target.dataset.index;
+    event.target.classList.add('active');
+    currentSelected = event.target;
+    carouselImgDiv.textContent = '';
+    const carouselImg = document.createElement("img");
+    carouselImg.src = imagePaths[currentIndex];
+    carouselImgDiv.appendChild(carouselImg);
+}
+
 
 function next(){
 
+    currentSelected.classList.remove('active');
+    
+
     currentIndex = (currentIndex + 1) % imagePaths.length;
+    currentSelected = carouselSelectionsDiv.children[currentIndex];
+    currentSelected.classList.add('active');
     carouselImgDiv.textContent = '';
     const carouselImg = document.createElement("img");
     carouselImg.src = imagePaths[currentIndex];
@@ -37,7 +66,10 @@ function next(){
 }
 
 function prev(){
+    currentSelected.classList.remove('active');
     currentIndex = (currentIndex - 1 + imagePaths.length) % imagePaths.length;
+    currentSelected = carouselSelectionsDiv.children[currentIndex];
+    currentSelected.classList.add('active');
     carouselImgDiv.textContent = '';
     const carouselImg = document.createElement("img");
     carouselImg.src = imagePaths[currentIndex];
